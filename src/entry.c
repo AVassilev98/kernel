f07154c584efd49369933e8c8ff508a68c78ed17
Anton Vassilev <avvassil@uwaterloo.ca>
2021-12-17 19:33:20 -0500

    Initial commit

---
+++ src/entry.c
@@ -0,0 +1,16 @@
+#include "bwio/bwio.h"
+#include "ts7200.h"
+int _start(int argc, char *argv[])
+{
+    char str[] = "Hello\n\r";
+    bwsetfifo(COM2, OFF);
+    bwputstr(COM2, str);
+    bwputw(COM2, 10, '*', str);
+    bwprintf(COM2, "Hello world.\n\r");
+    bwprintf(COM2, "%s world%u.\n\r", "Well, hello", 23);
+    bwprintf(COM2, "%d worlds for %u person.\n\r", -23, 1);
+    bwprintf(COM2, "%x worlds for %d people.\n\r", -23, 723);
+    str[0] = bwgetc(COM2);
+    bwprintf(COM2, "%s", str);
+    return 0;
+}
