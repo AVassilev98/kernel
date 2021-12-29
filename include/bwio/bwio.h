c8a0e4dd7595b3811cce822396a56ec29e124c10
Anton Vassilev <avvassil@uwaterloo.ca>
2021-12-29 01:35:04 -0500

    Add include header for io header

--- include/bwio/bwio.h
+++ include/bwio/bwio.h
@@ -1,39 +1,38 @@
 /*
  * bwio.h
  */
+#pragma once
 
 typedef char *va_list;
 
-#define __va_argsiz(t)	\
-		(((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+#define __va_argsiz(t) (((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
 
-#define va_start(ap, pN) ((ap) = ((va_list) __builtin_next_arg(pN)))
+#define va_start(ap, pN) ((ap) = ((va_list)__builtin_next_arg(pN)))
 
-#define va_end(ap)	((void)0)
+#define va_end(ap) ((void)0)
 
-#define va_arg(ap, t)	\
-		 (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
+#define va_arg(ap, t) (((ap) = (ap) + __va_argsiz(t)), *((t *)(void *)((ap)-__va_argsiz(t))))
 
-#define COM1	0
-#define COM2	1
+#define COM1 0
+#define COM2 1
 
-#define ON	1
-#define	OFF	0
+#define ON 1
+#define OFF 0
 
-int bwsetfifo( int channel, int state );
+int bwsetfifo(int channel, int state);
 
-int bwsetspeed( int channel, int speed );
+int bwsetspeed(int channel, int speed);
 
-int bwputc( int channel, char c );
+int bwputc(int channel, char c);
 
-int bwgetc( int channel );
+int bwgetc(int channel);
 
-int bwputx( int channel, char c );
+int bwputx(int channel, char c);
 
-int bwputstr( int channel, char *str );
+int bwputstr(int channel, char *str);
 
-int bwputr( int channel, unsigned int reg );
+int bwputr(int channel, unsigned int reg);
 
-void bwputw( int channel, int n, char fc, char *bf );
+void bwputw(int channel, int n, char fc, char *bf);
 
-void bwprintf( int channel, char *format, ... );
+void bwprintf(int channel, char *format, ...);
