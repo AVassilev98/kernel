70eb2c5bcef4d07f051c6ce52db8166817dafaa0
Anton Vassilev <avvassil@uwaterloo.ca>
2021-12-29 02:17:52 -0500

    Inline struct functions
    implementations need to be moved to header to allow for inlining
    Update inline notation

--- kernel/scheduler_ring_buffer.h
+++ kernel/scheduler_ring_buffer.h
@@ -3,6 +3,8 @@
 #include "common_defs.h"
 #include "scheduler.h"
 
+/*############ DECLARATIONS ################*/
+
 typedef struct SchedulerRingBuffer
 {
     uint8_t head_index;
@@ -11,8 +13,42 @@ typedef struct SchedulerRingBuffer
     TaskDescriptor *elems[MAX_TASKS];
 } SchedulerRingBuffer;
 
-void scheduler_ring_buffer_init(SchedulerRingBuffer *srb);
-bool scheduler_ring_buffer_full(SchedulerRingBuffer *srb);
-bool scheduler_ring_buffer_empty(SchedulerRingBuffer *srb);
-void scheduler_ring_buffer_elem_push(SchedulerRingBuffer *srb, TaskDescriptor *task);
-TaskDescriptor *scheduler_ring_buffer_elem_pop(SchedulerRingBuffer *srb);
+static void scheduler_ring_buffer_init(SchedulerRingBuffer *srb) __attribute__((always_inline));
+static bool scheduler_ring_buffer_full(SchedulerRingBuffer *srb) __attribute__((always_inline));
+static bool scheduler_ring_buffer_empty(SchedulerRingBuffer *srb) __attribute__((always_inline));
+static void scheduler_ring_buffer_elem_push(SchedulerRingBuffer *srb, TaskDescriptor *task) __attribute__((always_inline));
+static TaskDescriptor *scheduler_ring_buffer_elem_pop(SchedulerRingBuffer *srb) __attribute__((always_inline));
+
+/*############ DEFINITIONS ################*/
+
+static void scheduler_ring_buffer_init(SchedulerRingBuffer *srb)
+{
+    srb->num_elems = 0;
+    srb->head_index = 0;
+    srb->tail_index = 0;
+}
+
+static inline bool scheduler_ring_buffer_full(SchedulerRingBuffer *srb)
+{
+    return srb->num_elems == MAX_TASKS;
+}
+
+static inline bool scheduler_ring_buffer_empty(SchedulerRingBuffer *srb)
+{
+    return srb->num_elems == 0;
+}
+
+static inline void scheduler_ring_buffer_elem_push(SchedulerRingBuffer *srb, TaskDescriptor *task)
+{
+    srb->num_elems++;
+    srb->elems[srb->tail_index++] = task;
+    srb->tail_index &= (MAX_TASKS - 1);
+}
+
+static TaskDescriptor *scheduler_ring_buffer_elem_pop(SchedulerRingBuffer *srb)
+{
+    srb->num_elems--;
+    TaskDescriptor *ret_task = srb->elems[srb->head_index++];
+    srb->head_index &= (MAX_TASKS - 1);
+    return ret_task;
+}
--- kernel/task_table.h
+++ kernel/task_table.h
@@ -1,5 +1,9 @@
 #pragma once
+#include "bwio/bwio.h"
 #include "scheduler_ring_buffer.h"
+#include "ts7200.h"
+
+/*############ DECLARATIONS ################*/
 
 typedef struct TaskTable
 {
@@ -7,6 +11,68 @@ typedef struct TaskTable
     SchedulerRingBuffer table[NUM_PRIORITY];
 } TaskTable;
 
-void task_table_init(TaskTable *tt);
-TaskDescriptor *task_table_elem_exchange(TaskTable *tt, TaskDescriptor *td);
-void task_table_elem_add(TaskTable *tt, TaskDescriptor *td);
+static void task_table_init(TaskTable *tt) __attribute__((always_inline));
+static TaskDescriptor *task_table_elem_exchange(TaskTable *tt, TaskDescriptor *td) __attribute__((always_inline));
+static void task_table_elem_add(TaskTable *tt, TaskDescriptor *td) __attribute__((always_inline));
+
+/*############ DEFINITIONS ################*/
+
+#define ROW_TO_MASK(row) (0x8000 >> row)
+#define TABLE_MASK_INIT(mask) (mask = 1 << 31)
+#define TABLE_EMPTY(mask) (mask == 1 << 31)
+
+static void task_table_init(TaskTable *tt)
+{
+    uint32_t i;
+    for (i = 0; i < MAX_TASKS; i++)
+    {
+        scheduler_ring_buffer_init(&tt->table[i]);
+    }
+    TABLE_MASK_INIT(tt->table_mask);
+}
+
+static TaskDescriptor *task_table_elem_exchange(TaskTable *tt, TaskDescriptor *td)
+{
+    static const int MultiplyDeBruijnBitPosition2[32] = {15, 14, 28, 13, 29, 1,  24, 12, 30, 22, 20, 0, 25, 17, 11, 7,
+                                                         31, 27, 2,  23, 21, 19, 16, 8,  26, 3,  18, 9, 4,  10, 5,  6};
+
+    uint32_t mask_lsb = (tt->table_mask) & (-tt->table_mask);
+
+    if (td != NULL)
+    {
+        uint32_t priority = td->priority;
+        uint32_t active_mask = ROW_TO_MASK(priority);
+        if (active_mask < mask_lsb)
+        {
+            return td;
+        }
+
+        tt->table_mask |= active_mask;
+        SchedulerRingBuffer *srb = &tt->table[priority];
+        scheduler_ring_buffer_elem_push(srb, td);
+    }
+
+    if (TABLE_EMPTY(mask_lsb))
+    {
+        return NULL;
+    }
+
+    uint32_t new_task_priority = MultiplyDeBruijnBitPosition2[(uint32_t)(mask_lsb * 0x077CB531U) >> 27];
+
+    SchedulerRingBuffer *srb = &tt->table[new_task_priority];
+    TaskDescriptor *ret = scheduler_ring_buffer_elem_pop(srb);
+
+    if (scheduler_ring_buffer_empty(srb))
+    {
+        tt->table_mask &= ~mask_lsb;
+    }
+    return ret;
+}
+
+static void task_table_elem_add(TaskTable *tt, TaskDescriptor *td)
+{
+    SchedulerRingBuffer *srb = &tt->table[td->priority];
+    scheduler_ring_buffer_elem_push(srb, td);
+    tt->table_mask |= ROW_TO_MASK(td->priority);
+    bwprintf(COM2, "tt->table_mask: %x\r\n", tt->table_mask);
+}
